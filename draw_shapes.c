3b2ccee56b915ff2c892aa8d45b5ddb1c27ebac3
evelynnguera <evelynnguerra@gmail.com>
2024-02-06 17:25:41 -0700

    updating the main

--- draw_shapes.c
+++ draw_shapes.c
@@ -25,4 +25,14 @@ void print_triangle(int leftCol, int size)
     putchar('\n');
   }
 }
+  void print_arrow(int leftCol, int size)
+  {
+    for (int row 0 ; row <= size; row++){
+      int minCol = leftCol + size - row, maxCol = leftCol + size +row;
+      int col;
+      for (col = 0; col < minCol; col++) putchar(' ');
+      for(        ; col <= maxCol; col++) putchar('*');
+      putchar('\n');
+  }
+}
 
